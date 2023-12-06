0209c25211760f1466bcc9957fb006389251f7b0
Ludmila Carvalho <ludmilamsaid@gmail.com>
2023-12-05 22:48:15 -0300

    Auto stash before merge of "main" and "origin/main"
    including some features to the menu

--- main.cpp
+++ main.cpp
@@ -9,10 +9,12 @@ int main() {
     // Vector para armazenar atividades (projetos e tarefas)
     std::vector<Atividade*> atividades;
 
+    // Criar um usuário
     Usuario usuario("user1", "user1@example.com", "password");
 
     // Loop principal
     while (true) {
+        // Exibir opções de ação
         std::cout << "Selecione uma ação:" << std::endl;
         std::cout << "1. Criar projeto" << std::endl;
         std::cout << "2. Mostrar projetos" << std::endl;
@@ -20,18 +22,32 @@ int main() {
         std::cout << "4. Mostrar tarefas" << std::endl;
         std::cout << "5. Sair" << std::endl;
         int escolha;
-        std::cin >> escolha;
-
-
-        if (escolha == 1) {
-            cout << "Digite o título do projeto: ";
-            string titulo;
-            cin.ignore();
-            getline(cin, titulo);
-            projetos.push_back(Projeto(titulo));
-            cout << "Projeto criado com sucesso!" << endl;
-        }
+        std::cin >> escolha;
+        // Executar ação com base na escolha do usuário
+            if (escolha == 1) {
+                // Criar um novo projeto
+                std::cout << "Digite o título do projeto: ";
+                std::string titulo;
+                std::cin.ignore();
+                getline(std::cin, titulo);
+                Projeto* projeto = new Projeto(titulo);
+                atividades.push_back(projeto);
+                std::cout << "Projeto criado com sucesso!" << std::endl;
+            } else if (escolha == 2) {
+                // Mostrar detalhes de todos os projetos disponíveis
+                std::cout << "Projetos disponíveis:" << std::endl;
+                for (size_t i = 0; i < atividades.size(); i++) {
+                    atividades[i]->exibirDetalhes();
+                }
+            } else if (escolha == 3) {
+                // Criar uma nova tarefa
+                std::cout << "Digite o título da tarefa: ";
+                std::string tituloT;
+                std::cin.ignore();
+                getline(std::cin, tituloT);
+                Tarefa* tarefa = new Tarefa(tituloT);
+                atividades.push_back(tarefa);
+                std::cout << "Tarefa criada com sucesso!" << std::endl;
+            }
     }
-    
-    return 0;
 }
